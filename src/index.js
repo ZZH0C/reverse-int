4ea0e2a3467a946c7ad9c634666c1a3e55e7ac16
zzh0c <zzh0czz@gmail.com>
2020-03-20 23:10:45 +0300

    init: create task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    if (typeof n !== "number"){throw "Error!";}
+    let aArr=n.toString().split("").reverse();
+    aArr=aArr.join("");
+    aArr=parseInt(aArr,10);
+    return aArr;
+};
